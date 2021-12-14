f6139e23bfb035bb7b8ca738e74a6dea6051e801
贺全喜 <hequanxi@bianfeng.com>
2021-12-14 10:09:01 +0800

    增加一种新解法

--- 回文数.go
+++ 回文数.go
@@ -1,12 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 // @solution-sync:begin
 // 解题思路：
 // 1. 根据回文数特点，负数不可能，判断后提前返回
 // 2. 对数字进行求余操作，将个个数字放入切片中，然后对切片遍历对比
-func isPalindrome(x int) bool {
+func isPalindrome01(x int) bool {
 	if x < 0 {
 		return false
 	}
@@ -29,9 +32,30 @@ func isPalindrome(x int) bool {
 
 // @solution-sync:end
 
+// @solution-sync:begin
+// 解题思路：
+// 1. 根据回文数特点，负数不可能，判断后提前返回
+// 2. 将数字转为字符串，然后对字符串进行遍历对比
+func isPalindrome02(x int) bool {
+	if x < 0 {
+		return false
+	}
+
+	str := strconv.Itoa(x)
+	l := len(str)
+	for i := 0; i < l/2; i++ {
+		if str[i] != str[l-i-1] {
+			return false
+		}
+	}
+	return true
+}
+
+// @solution-sync:end
+
 func main() {
 	var x = 121
 
-	var result = isPalindrome(x)
+	var result = isPalindrome02(x)
 	fmt.Printf("%v\n", result)
 }
